92472c8328499df58f4ef8a8c11fd4d31cb96164
Matheus Bertol <matheusbertol76@gmail.com>
2023-10-29 22:06:10 -0300

    Rota para obter uma locação específica por ID (GET) e para atualizar a data de finalização de uma locação (PUT)

--- src/main.py
+++ src/main.py
@@ -32,3 +32,21 @@ def nova_locacao(locacao: Locacao):
 @app.get("/locacoes", response_model=List[Locacao])
 def get_locacoes():
     return locacoes_db
+
+#GET /locacoes/id (locação individual)
+@app.get("/locacoes/{locacao_id}", response_model=Locacao)
+def get_locacao(locacao_id: int):
+    for loc in locacoes_db:
+        if loc.id == locacao_id:
+            return loc
+    raise HTTPException(status_code=404, detail="Locação não encontrada!")
+
+#PUT /locacoes/id (locação individual)
+@app.put("/locacoes/{locacao_id}", response_model=Locacao)
+def update_locacao(locacao_id: int, locacao: Locacao):
+    for loc in locacoes_db:
+        if loc.id == locacao_id:
+            # Atualize a data de finalização da locação
+            loc.data_finalizacao = locacao.data_finalizacao
+            return loc
+    raise HTTPException(status_code=404, detail="Locação não encontrada!")
